0a80e79406a70fc8548ad053f74c39e26bc5d5df
Craig Skinfill <223300+cskinfill@users.noreply.github.com>
2025-01-03 11:39:46 -0500

    working

--- app.py
+++ app.py
@@ -8,24 +8,28 @@ metrics = PrometheusMetrics(app)
 
 metrics.info('app_info', 'Python Hacking', version='0.0.1')
 
+
 def db_conn():
     conn = sqlite3.connect("services.db")
     conn.row_factory = sqlite3.Row
     return conn
 
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
 
 @app.route("/services")
 def services(methods=["GET"]):
     data = []
     with closing(db_conn()) as conn:
         cur = conn.cursor()
-        cur.execute(f"SELECT * FROM services")
+        cur.execute("SELECT * FROM services")
         for row in cur.fetchall():
             data.append(dict(row))
     return jsonify(data)
+
 
 @app.route("/service/<id>")
 def service(id, methods=["GET"]):
@@ -34,7 +38,7 @@ def service(id, methods=["GET"]):
         cur.execute(f"SELECT * FROM services WHERE id={id}")
         for row in cur.fetchall():
             return jsonify(dict(row))
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
